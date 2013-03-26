3f460987250ecb2e806f3d46af36df44810251f7
nonprofitgibi <gibispace@gmail.com>
2013-03-25 23:17:02 -0300

    Create mcserver.py

---
+++ mcserver.py
@@ -0,0 +1,92 @@
+print "Minecraft Server install script"
+
+print "would you like to install Minecraft Server?"
+
+#ask user for input. if yes continue, if no close
+
+print "Vannillia is the origional Server straight from minecraft.net, Bukkit is oriented towards modders. if you are unsure choose vanilla"
+
+print "(1.Vanilla) (2.Bukkit)"
+
+#ask for user input
+#if user chose vanillia download from minecraft.net
+#if user chose Bukkit download from Bukkit.org
+
+#check to see if Java is installed
+  #if no install java
+
+#ask for user input - Name of Server Admin
+
+print "If you are unsure of whitelist choose no"
+#ask for user input - Enable whitelist?
+	if yes
+	#ask for user input - names for white list
+
+#ask for user input - enable Monsters?
+
+#ask for user input - Enable Animals?
+
+#ask for user input - enable NPC's
+
+#ask for user input - Generate structures?
+
+#ask for user input - pvp?
+
+print "Only disable this option if you will be playing without internet"
+#ask for user input - online mode?
+
+print "if a character dies their banned from the server"
+#ask for user input - hardcore?
+
+#ask for user input - enable pvp?
+
+print "if not sure set to 25565"
+#ask for user input - server port?
+
+print "leave blank if not sure"
+#ask for user input - enable rcon?
+
+print "used for generating maps, if unsure leave blank"
+#ask for user input - Map seed?
+
+print "URL link for Texture pack download, if unsure leave blank"
+#ask for user input - Texture pack?
+
+print "if unsure leave blank"
+#ask for user input - enable query?
+
+#ask for user input - difficulty
+
+print "(1.survival) (2.creative)"
+#ask for user input - gamemode
+
+print "(1.easy) (2.normal) (3.hard)"
+#ask for user input - difficulty
+
+print "Message to apear on server list"
+#ask for user input - MOTD
+
+print "reccomended 256"
+#ask for user input - Max build height
+
+print "(1.default) (2.flatland) default reccomended"
+#ask for user input - Level type?
+
+print "if not sure leave blank"
+#ask for user input - generator settings?
+
+print "reccomended 10"
+#ask for unser input - view distance?
+
+#Ask for user input - generate structures
+
+#write to file ./server.properties & ./whitelist & ./op
+
+#create batch/bash script in server directory to start server
+#create desktop shortcut with icon to startup script
+
+print "would you like to run your server now?"
+	if yes
+	start startup script
+
+exit
