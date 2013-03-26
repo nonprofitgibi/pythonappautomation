46d554477b38b8f53380e4c5680bddac1f8774cd
Kenneth Andrews <techfighterminal@gmail.com>
2013-03-26 17:27:20 -0400

    touched up the code

--- mcserver.py
+++ mcserver.py
@@ -1,91 +1,94 @@
+#
+# title		Minecraft Server Automation Script
+#
+# description	Script to automate the installation of a minecraft server, and Java
+#
+# authors	Kenneth Andrews, Tanner Gibson
+#
+# licence	GPL.V2
+#..........................................................................................
+
 print "Minecraft Server install script"
 
 print "would you like to install Minecraft Server?"
 
-#ask user for input. if yes continue, if no close
+install = raw_input ("would you like to install minecraft server? (y)es (n)o: ")
+#if yes continue, if no close
 
 print "Vannillia is the origional Server straight from minecraft.net, Bukkit is oriented towards modders. if you are unsure choose vanilla"
+server_type = raw_input ("Which version would you like to download? 1-2: ")
 
-print "(1.Vanilla) (2.Bukkit)"
-
-#ask for user input
 #if user chose vanillia download from minecraft.net
 #if user chose Bukkit download from Bukkit.org
 
 #check to see if Java is installed
-  #if no install java
+	#if no install java
 
-#ask for user input - Name of Server Admin
+admin = raw_input("Name of Admin?: ")
 
 print "If you are unsure of whitelist choose no"
-#ask for user input - Enable whitelist?
+whitelist = raw_input("enable whitelist?")
 	if yes
 	#ask for user input - names for white list
 
-#ask for user input - enable Monsters?
+monsters = raw_input("enable monsters?: ")
 
-#ask for user input - Enable Animals?
+animals = raw_input("spawn animals?: ")
 
-#ask for user input - enable NPC's
+npc = raw_input("spawn NPCs?: ")
 
-#ask for user input - Generate structures?
+structures = raw_input("generate structures?: ")
 
-#ask for user input - pvp?
+pvp = raw_input("enable pvp?: ")
 
 print "Only disable this option if you will be playing without internet"
-#ask for user input - online mode?
+online = raw_input("enable online mode?: ")
 
 print "if a character dies their banned from the server"
-#ask for user input - hardcore?
-
-#ask for user input - enable pvp?
+hardcore = raw_input("enable hardcore mode?: ")
 
 print "if not sure set to 25565"
-#ask for user input - server port?
+port = raw_input("port number?: ")
 
-print "leave blank if not sure"
-#ask for user input - enable rcon?
+print "disable if not sure"
+rcon = raw_input("enable rcon?: ")
 
 print "used for generating maps, if unsure leave blank"
-#ask for user input - Map seed?
+seed = raw_input("map seed?: ")
 
 print "URL link for Texture pack download, if unsure leave blank"
-#ask for user input - Texture pack?
+texture = raw_input("texture pack?: ")
 
-print "if unsure leave blank"
-#ask for user input - enable query?
-
-#ask for user input - difficulty
+print "if unsure disable"
+query = raw_input("enable query?: ")
 
 print "(1.survival) (2.creative)"
-#ask for user input - gamemode
+gamemode = raw_input("gamemode?: ")
 
 print "(1.easy) (2.normal) (3.hard)"
-#ask for user input - difficulty
+difficulty = raw_input("difficulty? 1-3")
 
 print "Message to apear on server list"
-#ask for user input - MOTD
+motd = raw_input("MODT?: ")
 
 print "reccomended 256"
-#ask for user input - Max build height
+build = raw_input("build height?: ")
 
 print "(1.default) (2.flatland) default reccomended"
-#ask for user input - Level type?
+Map_type = raw_input("map type? 1-2: ")
 
 print "if not sure leave blank"
-#ask for user input - generator settings?
+generator_settings = raw_input("generator settings?: ")
 
 print "reccomended 10"
-#ask for unser input - view distance?
-
-#Ask for user input - generate structures
+view = raw_input("view distance?: ")
 
 #write to file ./server.properties & ./whitelist & ./op
 
 #create batch/bash script in server directory to start server
 #create desktop shortcut with icon to startup script
 
-print "would you like to run your server now?"
+start_server = raw_input("Would you like to start your server now? (y)es (n)o: ")
 	if yes
 	start startup script
 
